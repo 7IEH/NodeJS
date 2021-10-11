7a23b890c6231da3d15fe42f6d9c3ac205bcff16
7IEH <80614927+7IEH@users.noreply.github.com>
2021-10-12 03:11:13 +0900

    FrontDesign

--- UMAMUSI_Module/routes/index.js
+++ UMAMUSI_Module/routes/index.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var router = express.Router();
+var fs=require('fs');
 
 router.get('/',function(req,res){
-    res.render('home/welcome');
+    res.render('home/main');
 });
 router.get('/about',function(req,res){
     res.render('home/about');
